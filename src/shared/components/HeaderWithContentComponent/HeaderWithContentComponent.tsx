e1fc41fd133501c267abebc299f40271ace67152
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-03-02 16:01:43 -0600

    feat: Add HeaderWithContentComponent. (#1)

---
+++ src/shared/components/HeaderWithContentComponent/HeaderWithContentComponent.tsx
@@ -0,0 +1,39 @@
+import HeaderComponent from "../HeaderComponent/HeaderComponent.component";
+
+type position = 'center' | 'start' | 'end';
+
+interface HeaderWithContentComponentProps {
+    level: 2 | 3 | 4;
+    children?: React.ReactNode;
+    title: string;
+    content: string;
+    className?: string;
+    positionText?: position;
+}
+
+const positionClasses: Record<position, string> = {
+    'center': 'justify-center text-center items-center',
+    'start': 'justify-start text-start',
+    'end': 'justify-end text-end',
+}
+
+function HeaderWithContentComponent({level, children, content, title, className, positionText = 'start'}: HeaderWithContentComponentProps) {
+    const globalClasses = 'w-full';
+    return (
+        <div className={ `${children != null? 'flex items-center': ''} ${className ?? ''} ${globalClasses}`}>
+            <div className={`flex flex-col ${positionClasses[positionText]} flex-1`}>
+                <HeaderComponent level={level}>
+                    {title}
+                </HeaderComponent>
+                <p>
+                    {content}
+                </p>
+            </div>
+            <div>
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default HeaderWithContentComponent;
