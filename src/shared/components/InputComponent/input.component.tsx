3d410302cdb5f7a6283906bbcf8bdc11aa37133b
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-03-02 16:06:55 -0600

    fix: Fix bugs on the InputComponent showing errors of name paths and now it shows if a field is optional or not.

--- src/shared/components/InputComponent/input.component.tsx
+++ src/shared/components/InputComponent/input.component.tsx
@@ -1,21 +1,23 @@
-import type { FieldErrors, FieldValues, Path, RegisterOptions, UseFormRegister } from "react-hook-form";
+import { get, type FieldErrors, type FieldValues, type Path, type RegisterOptions, type UseFormRegister } from "react-hook-form";
 
 interface InputComponentProps<T extends FieldValues> {
     label: string;
     name: Path<T>;
     register: UseFormRegister<T>;
     errors: FieldErrors<T>;
-    type: 'text' | 'number';
+    type: 'text' | 'number' | 'date';
     validations?: RegisterOptions<T, Path<T>>;
 }
 
 function InputComponent<T extends FieldValues>({label,type, name, register, errors, validations = {}}: InputComponentProps<T>) {
+    const required: boolean = Object.keys(validations).includes('required');
+    const error = get(errors, name);
     return (
         <fieldset className="fieldset">
-            <legend className="fieldset-legend">{label}</legend>
+            <legend className="fieldset-legend flex items-center">{label} {required && <p className="label text-error">*</p>}</legend>
                 <input {...register(name, validations)} type={type} className="input" placeholder="Type here" />
-                {/* {!validations.required && <p className="label">Optional</p>} */}
-                {errors[name] && <span className="text-error text-xs">{String(errors[name].message)}</span>}
+                {!required && <p className="label">Opcional</p>}
+                {error && <span className="text-error text-xs">{String(error.message)}</span>}
         </fieldset>
     );
 }
