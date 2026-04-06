570ca306facd6d1d711b39ff9e189da79ce038d7
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-04-06 12:06:58 -0600

    refactor: Enhance createCv function to accept user image and options for format and language.

--- src/CV/requests/CVRequests.ts
+++ src/CV/requests/CVRequests.ts
@@ -1,7 +1,29 @@
 import axios from "axios";
 import type { CreateCvFormOutput } from "../components/CreateCvForm/schemas/CreateCVSchema";
 
-export function createCv(body: CreateCvFormOutput) {
-    console.log(import.meta.env.VITE_API_BASE_URL);
-    return axios.post<Blob>(`${import.meta.env.VITE_API_BASE_URL}/curriculum-vitae`, body, {responseType: 'blob'});
+type cvFormatTypes = 'Harvard' | 'Ats' | 'Modern';
+type cvLanguages = 'en' | 'es';
+
+interface createCvOptions {
+    type?: cvFormatTypes;
+    language?: cvLanguages;
+}
+
+export function createCv(body: CreateCvFormOutput, userImage: Blob, opts: Partial<createCvOptions>) {
+    const params = {
+        type: opts.type ?? 'Harvard',
+        lang: opts.language ?? 'es',
+    }
+
+    const formData = new FormData();
+    formData.append('data', JSON.stringify(body));
+    formData.append('picture', userImage);
+
+    return axios.post<Blob>(
+        `${import.meta.env.VITE_API_BASE_URL}/curriculum-vitae`, 
+        formData, 
+        {
+            params: params,
+            responseType: 'blob'
+        });
 }
