878971719d7e67656926b12c186a3c2bd6fc5ee0
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-04-06 12:22:01 -0600

    refactor: Update createCv function to use default image when user image is not provided.

--- src/CV/requests/CVRequests.ts
+++ src/CV/requests/CVRequests.ts
@@ -1,15 +1,16 @@
 import axios from "axios";
 import type { CreateCvFormOutput } from "../components/CreateCvForm/schemas/CreateCVSchema";
+import { getDefaultImage } from "../../shared/utils/getDefaultImage";
 
 type cvFormatTypes = 'Harvard' | 'Ats' | 'Modern';
 type cvLanguages = 'en' | 'es';
 
 interface createCvOptions {
-    type?: cvFormatTypes;
-    language?: cvLanguages;
+    type: cvFormatTypes;
+    language: cvLanguages;
 }
 
-export function createCv(body: CreateCvFormOutput, userImage: Blob, opts: Partial<createCvOptions>) {
+export async function createCv(body: CreateCvFormOutput, userImage?: Blob, opts: Partial<createCvOptions> = {}) {
     const params = {
         type: opts.type ?? 'Harvard',
         lang: opts.language ?? 'es',
@@ -17,7 +18,7 @@ export function createCv(body: CreateCvFormOutput, userImage: Blob, opts: Partia
 
     const formData = new FormData();
     formData.append('data', JSON.stringify(body));
-    formData.append('picture', userImage);
+    formData.append('picture', userImage ?? await getDefaultImage());
 
     return axios.post<Blob>(
         `${import.meta.env.VITE_API_BASE_URL}/curriculum-vitae`, 
