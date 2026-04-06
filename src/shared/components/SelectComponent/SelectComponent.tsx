88c9b22e9eb61f2996091aa9272a060f1f01799c
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-04-06 12:57:40 -0600

    feat: Add select component.

---
+++ src/shared/components/SelectComponent/SelectComponent.tsx
@@ -0,0 +1,34 @@
+import { get, type FieldErrors, type FieldValues, type Path, type UseFormRegister } from "react-hook-form";
+
+export interface SelectOpt {
+    label: string;
+    value: string;
+}
+
+interface SelectComponentProps<T extends FieldValues>{
+    label: string;
+    register: UseFormRegister<T>;
+    name: Path<T>;
+    selectOptions: SelectOpt[];
+    isOptional: boolean,
+    errors: FieldErrors<T>
+}
+
+function SelectComponent<T extends FieldValues>({label, register, name, selectOptions = [], isOptional, errors}: SelectComponentProps<T>){
+    const error = get(errors, name);
+    return (
+        <fieldset className="fieldset">
+            <legend className="fieldset-legend">{label} {!isOptional && <span className="text-error">*</span>} </legend>
+                <select id={name} {...register(name)} className="select">
+                    <option disabled>Selecciona una opción</option>
+                    {
+                        selectOptions.map(({label, value}, index) => <option key={index} value={value}>{label}</option>)
+                    }
+                </select>
+            {error && <span className="text-error text-xs">{error.message}</span>}
+            {isOptional && <span className="label">Optional</span>}
+        </fieldset>
+    );
+}
+
+export default SelectComponent;
