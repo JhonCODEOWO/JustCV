4bb89ba6bd33b7d13cdcad11598ec0be92a9d2e0
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-04-13 16:45:20 -0600

    feat: Wrap routes with AppProvidersComponent for global context management

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,25 @@ import AboutUsComponentPage from './pages/AboutUsPageComponent/AboutUsPageCompon
 import CreatingCVPageComponent from './pages/CreatingCVPageComponent/CreatingCVPageComponent.component'
 import ListCvComponentPage from './CV/pages/ListCvComponentPage.component'
 import CvsProviderComponent from './CV/contexts/CvsContext/wrappers/CvsProviderComponent.component'
+import AppProvidersComponent from './wrappers/AppProviders.component'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='' element={<MainLayoutComponent/>}>
-            <Route index element={<IndexPageComponent/>}/>
-            <Route path='/aboutUs' element={<AboutUsComponentPage/>}/>
-            {/* Provider of cvs */}
-            <Route element={<CvsProviderComponent/>}>
-              <Route path='/creating-cv' element={<CreatingCVPageComponent/>}/>
-              <Route path='/home' element={<ListCvComponentPage/>}/>
-            </Route>
-        </Route>
-      </Routes>
+    {/* Providers globales */}
+      <AppProvidersComponent>
+        <Routes>
+          <Route path='' element={<MainLayoutComponent/>}>
+              <Route index element={<IndexPageComponent/>}/>
+              <Route path='/aboutUs' element={<AboutUsComponentPage/>}/>
+              {/* Provider of cvs */}
+              <Route element={<CvsProviderComponent/>}>
+                <Route path='/creating-cv' element={<CreatingCVPageComponent/>}/>
+                <Route path='/home' element={<ListCvComponentPage/>}/>
+              </Route>
+          </Route>
+        </Routes>
+      </AppProvidersComponent>
     </BrowserRouter>
   )
 }
