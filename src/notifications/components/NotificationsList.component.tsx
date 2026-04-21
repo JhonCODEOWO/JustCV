1e654a77f9187c6d002ab812c7e37e22c71294ef
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-04-21 13:53:24 -0600

    fix: Now the notification container now shows at top level in z index.

--- src/notifications/components/NotificationsList.component.tsx
+++ src/notifications/components/NotificationsList.component.tsx
@@ -6,7 +6,7 @@ function NotificationsListComponent() {
     const {notifications, deleteNotification} = useNotificationsContext();
     
     return createPortal(
-        <div className="px-3 py-5 flex flex-col gap-y-2 top-0 left-[50%] translate-x-[-50%] fixed">
+        <div className="px-3 py-5 flex flex-col gap-y-2 top-0 left-[50%] translate-x-[-50%] fixed z-50">
             {notifications
                 .map(n => (
                     <NotificationElementComponent key={n.id} notification={n} onRemoveElement={deleteNotification}/>
