a38d7e8577fbda38cfbae3778cbdbba44a25bf4a
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-05-11 16:59:53 -0600

    refactor: Use new options component to list every option of a cv element.

--- src/CV/components/CvElementComponent/CvElementComponent.tsx
+++ src/CV/components/CvElementComponent/CvElementComponent.tsx
@@ -9,6 +9,7 @@ import { DownloadOptions, type FormData } from "./schemas/download-options.schem
 import type { DownloadOptionsElementInterface } from "./interfaces/DownloadOptionsElementInterface.interface";
 import type { CvElementDownloadArgsInterface } from "./interfaces/CvElementDownloadArgsInterface.interface";
 import type { CvElementContext } from "../../contexts/CvsContext/interfaces/CvElementContext.interface";
+import OptionsComponent from "../../../shared/components/OptionsComponent/OptionsComponent.component";
 
 
 interface CvElementComponentProps {
@@ -122,18 +123,21 @@ function CvElementComponent({ element, index, onDeleteBtn, onDownloadBtn, onUpda
                 <p>{cv.fullname}</p>
                 <p className="underline">{cv.email}</p>
             </div>
-            <p className="wrap-break-word text-sm">{cutString(cv.resume, {endIndex: 50})}</p>
-            <div className="flex flex-row-reverse justify-evenly">
-                <button className="text-error cursor-pointer tooltip tooltip-bottom" onClick={() => onDeleteBtn(index)} data-tip="Borrar">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7 21q-.825 0-1.412-.587T5 19V6q-.425 0-.712-.288T4 5t.288-.712T5 4h4q0-.425.288-.712T10 3h4q.425 0 .713.288T15 4h4q.425 0 .713.288T20 5t-.288.713T19 6v13q0 .825-.587 1.413T17 21zm3.713-4.288Q11 16.426 11 16V9q0-.425-.288-.712T10 8t-.712.288T9 9v7q0 .425.288.713T10 17t.713-.288m4 0Q15 16.426 15 16V9q0-.425-.288-.712T14 8t-.712.288T13 9v7q0 .425.288.713T14 17t.713-.288"/></svg>
-                </button>
-                <button className="text-info cursor-pointer tooltip tooltip-bottom" onClick={() => onUpdateCv(uuid)} data-tip="Editar">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M9 15v-4.25L19.625.125L23.8 4.4L13.25 15zm10.6-9.2l1.425-1.4l-1.4-1.4L18.2 4.4zM3 21V3h10.925L7 9.925V17h7.05L21 10.05V21z"/></svg>
-                </button>
-                <button className="text-success cursor-pointer tooltip tooltip-bottom" onClick={() => setShowModal(true)} data-tip="Generar PDF...">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m12 17l4-4l-1.4-1.4l-1.6 1.55V9h-2v4.15L9.4 11.6L8 13zm-8 5V8l6-6h10v20z"/></svg>
-                </button>
-            </div>
+            <p className="wrap-break-word text-sm">{cutString(cv.resume, {endIndex: 75})}</p>
+                <OptionsComponent>
+                    <button className="text-error cursor-pointer flex items-center gap-x-3" onClick={() => onDeleteBtn(index)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7 21q-.825 0-1.412-.587T5 19V6q-.425 0-.712-.288T4 5t.288-.712T5 4h4q0-.425.288-.712T10 3h4q.425 0 .713.288T15 4h4q.425 0 .713.288T20 5t-.288.713T19 6v13q0 .825-.587 1.413T17 21zm3.713-4.288Q11 16.426 11 16V9q0-.425-.288-.712T10 8t-.712.288T9 9v7q0 .425.288.713T10 17t.713-.288m4 0Q15 16.426 15 16V9q0-.425-.288-.712T14 8t-.712.288T13 9v7q0 .425.288.713T14 17t.713-.288"/></svg>
+                            <p>Borrar elemento</p>
+                        </button>
+                        <button className="text-info cursor-pointer flex items-center gap-x-3" onClick={() => onUpdateCv(uuid)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M9 15v-4.25L19.625.125L23.8 4.4L13.25 15zm10.6-9.2l1.425-1.4l-1.4-1.4L18.2 4.4zM3 21V3h10.925L7 9.925V17h7.05L21 10.05V21z"/></svg>
+                            <p>Editar información...</p>
+                        </button>
+                        <button className="text-success cursor-pointer flex items-center gap-x-3" onClick={() => setShowModal(true)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m12 17l4-4l-1.4-1.4l-1.6 1.55V9h-2v4.15L9.4 11.6L8 13zm-8 5V8l6-6h10v20z"/></svg>
+                            <p>Descargar PDF...</p>
+                        </button>
+                </OptionsComponent>
         </div>
     );
 }
