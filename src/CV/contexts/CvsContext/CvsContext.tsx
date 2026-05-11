c846cfbc5dcd7512c522a38dcbd7a06b38bff650
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-05-11 16:58:13 -0600

    refactor: Add some docs to context type of CvsContext.

--- src/CV/contexts/CvsContext/CvsContext.tsx
+++ src/CV/contexts/CvsContext/CvsContext.tsx
@@ -3,10 +3,15 @@ import type { CreateCvFormBody } from "../../components/CreateCvForm/schemas/Cre
 import type { CvElementContext } from "./interfaces/CvElementContext.interface";
 
 interface CvsContextType {
+    /** All cvs inside provider */
     cvs: CvElementContext[],
+    /** Items left to save based on the limit inside the provider */
     itemsLeft: number;
+    /** Add a new item to the current data inside provider */
     addCv: (cv: CreateCvFormBody) => void;
+    /** Delete the item based on the index inside provider */
     deleteCv: (index: number) => void;
+    /** Update the item based on a existing uuid and replace it with the newCv arg data */
     updateCv: (uuid: string, newCv: CreateCvFormBody) => void;
 }
 
