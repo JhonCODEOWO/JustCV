2f7c5187ef0a8e3fab7e635b4cc0f43973ce236b
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-05-12 15:29:00 -0600

    fix: Improve UI for achievements section in CreateCvForm

--- src/CV/components/CreateCvForm/components/CreateAchievement/CreateAchievement.component.tsx
+++ src/CV/components/CreateCvForm/components/CreateAchievement/CreateAchievement.component.tsx
@@ -34,18 +34,25 @@ function CreateAchievementComponent({index, control, errors, register}: CreateAc
                 <HeaderComponent level={4}>
                 Logros destacados
                 </HeaderComponent>
-                {/* Botón de creación */}
-                <button type="button" className="btn btn-success rounded-full" onClick={() => appendAchievement({description: ''})} >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z"/></svg>
-                </button>
             </div>
             <p className="text-error text-xs text-center w-full">{errors.workExperience?.[index]?.achievements?.message}</p>
             {/* Renderización de elementos */}
-            <div className="md:grid md:grid-cols-2 md:h-39 overflow-y-auto justify-items-center items-center">
-                {achievements.length === 0 && <p className="col-span-3">No has añadido nada todavía</p>}
+            <div className="md:grid md:grid-cols-2 justify-items-center items-center">
+                {achievements.length === 0 
+                    && 
+                    <section className="col-span-3">
+                        <p>
+                            No has añadido nada todavía
+                        </p>
+                        <button type="button" className="btn btn-success rounded-full md:col-span-2" onClick={() => appendAchievement({description: ''})} >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z"/></svg>
+                            Añadir nuevo
+                        </button>
+                    </section>
+                }
                 {achievements.map((achievement, indexAchievement) => {
                     return (
-                        <div key={achievement.id} className="flex flex-col gap-x-3 items-center">
+                        <div key={achievement.id} className="flex gap-x-3 items-center">
                             <InputComponent<CreateCvFormBody> 
                                 errors={errors} 
                                 label="Descripción del logro" 
@@ -59,12 +66,18 @@ function CreateAchievementComponent({index, control, errors, register}: CreateAc
                                     }
                                 }
                             />
-                            <button className="text-xs underline text-error w-full text-start" type="button" onClick={() => removeAchievement(indexAchievement)}>
-                                Quitar logro
+                            <button className="text-xs underline text-error text-start" type="button" onClick={() => removeAchievement(indexAchievement)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m8.4 17l3.6-3.6l3.6 3.6l1.4-1.4l-3.6-3.6L17 8.4L15.6 7L12 10.6L8.4 7L7 8.4l3.6 3.6L7 15.6zm3.6 5q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22"/></svg>
                             </button>
                         </div>
                     )
                 })}
+                {achievements.length > 0 && (
+                    <button type="button" className="btn btn-success rounded-full md:col-span-2" onClick={() => appendAchievement({description: ''})} >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z"/></svg>
+                        Añadir nuevo
+                    </button>
+                )}
             </div>
         </div>
      );
