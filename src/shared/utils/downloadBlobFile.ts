8af382d04c77b1f9842fbae2889aebdc8105fa61
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-05-19 16:38:53 -0600

    fix: Now the method use the MIME type to define the format of the blob to download.

--- src/shared/utils/downloadBlobFile.ts
+++ src/shared/utils/downloadBlobFile.ts
@@ -5,11 +5,12 @@
 export function downloadBlobFile(blob: Blob, fileName: string = ''): void{
     const headString = `${Date.now().toString()}`;
     const finalString = `${headString}-${fileName}`;
+    const format = blob.type.split('/')[1];
 
     const a = document.createElement('a');
     const url = URL.createObjectURL(blob);
     a.href = url;
-    a.download = `${finalString}.${blob.type}`;
+    a.download = `${finalString}.${format}`;
     a.click();
 
     URL.revokeObjectURL(url);
