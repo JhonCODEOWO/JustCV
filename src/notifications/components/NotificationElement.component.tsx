1de0f04ce4bb2bb777ece5988aaae1f92233b6aa
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-05-20 12:42:49 -0600

    fix: The notifications now show the title correctly and has a width based on the viewport client.

--- src/notifications/components/NotificationElement.component.tsx
+++ src/notifications/components/NotificationElement.component.tsx
@@ -57,8 +57,10 @@ function NotificationElementComponent({notification, onRemoveElement}: Notificat
     return (
         <div className={`${!isRemoving? 'slideLeft': 'slideLeftOut'}`} ref={divRef} onMouseLeave={startTimer} onMouseEnter={handleMainMouseEnter}>
             <div className={`alert ${colorType[notification.type]} alert-soft flex justify-between`}>
-                {notification.title && <h4>{notification.title}</h4>}
-                <span>{notification.content}</span>
+                <div className="flex flex-col gap-y-1">
+                    {notification.title && <h4 className="font-bold">{notification.title}</h4>}
+                    <span>{notification.content}</span>
+                </div>
                 <button onClick={removeElement} className="cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4l-2.9 2.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l2.9-2.9l-2.9-2.875q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l2.9 2.9l2.875-2.9q.275-.275.7-.275t.7.275q.3.3.3.713t-.3.687L13.375 12l2.9 2.9q.275.275.275.7t-.275.7q-.3.3-.712.3t-.688-.3z"/></svg>
                 </button>
